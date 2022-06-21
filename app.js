75aa269c3373edacf78b73bac6eed07c5be5413a
rubylo718 <rubylo718@gmail.com>
2022-06-21 17:25:56 +0800

    docs: add views

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 require('./config/mongoose')
+const exphbs = require('express-handlebars')
 
 const port = process.env.PORT
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.render('index')
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+  res.render('edit')
 })
 
 app.listen(port, () => {
