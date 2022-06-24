b7af5d2c80da196d9c2c2161fb2d70094d9fdd0f
rubylo718 <rubylo718@gmail.com>
2022-06-24 22:24:56 +0800

    feat: add user auth

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const homeRoute = require('./modules/home')
 const userRoute = require('./modules/users')
 const expenseRoute = require('./modules/expense')
+const { authCheck }  = require('../middleware/authCheck')
 
 router.use('/users', userRoute)
-router.use('/expense', expenseRoute)
-router.use('/', homeRoute)
+router.use('/expense', authCheck, expenseRoute)
+router.use('/',authCheck, homeRoute)
 
 module.exports = router
