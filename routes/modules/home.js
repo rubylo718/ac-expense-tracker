a3085e0073678fe6bc62b1946e5ab4edb57ae1e9
rubylo718 <rubylo718@gmail.com>
2022-06-26 16:21:47 +0800

    feat: add filter category function in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,9 +6,14 @@ const Category = require('../../models/category-model')
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.find().lean().sort('id')
-
     const userId = req.user._id
-    const userRecords = await Record.find({ userId }).lean()
+
+    const filterCategoryValue = req.query.categoryFilter
+    const filterCategory = filterCategoryValue? await Category.findOne({ id: filterCategoryValue }).lean(): null
+    const filterCategoryId = filterCategory? filterCategory._id : null
+    const filterCategoryTitle = filterCategory? filterCategory.name : "全部類型"
+
+    const userRecords = filterCategory? await Record.find({ userId, categoryId: filterCategoryId }).lean() : await Record.find({ userId }).lean()
     let totalAmount = 0
     await Promise.all(
       userRecords.map(async record => {
@@ -17,10 +22,10 @@ router.get('/', async (req, res) => {
         totalAmount += record.amount
       })
     )
-    res.render('index', { categories, userRecords, totalAmount })
+    res.render('index', { categories, userRecords, totalAmount, filterCategoryTitle })
   } catch (error) {
     console.log(error)
   }
-})
+})     
 
 module.exports = router
