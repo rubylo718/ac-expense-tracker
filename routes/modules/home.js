3031667572bf3a2e68c65465a7aa489500676960
rubylo718 <rubylo718@gmail.com>
2022-07-18 22:37:32 +0800

    perf: update home module for less db calling to improve performance

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,21 +7,11 @@ router.get('/', async (req, res) => {
   try {
     const categories = await Category.find().lean().sort('id')
     const userId = req.user._id
-
-    const filterCategoryValue = req.query.categoryFilter
-    const filterCategory = filterCategoryValue ? await Category.findOne({ id: filterCategoryValue }).lean() : null
-    const filterCategoryId = filterCategory ? filterCategory._id : null
-    console.log('value', filterCategoryValue,typeof(filterCategoryValue))
-
-    const userRecords = filterCategory ? await Record.find({ userId, categoryId: filterCategoryId }).lean() : await Record.find({ userId }).lean()
+    const filterCategoryValue = req.query.categoryFilter  
+    const userAllRecords = await Record.find({ userId }).populate('userId').populate('categoryId').lean()
+    const userRecords = filterCategoryValue ? userAllRecords.filter(records => records.categoryId.id === filterCategoryValue) : userAllRecords
     let totalAmount = 0
-    await Promise.all(
-      userRecords.map(async record => {
-        const recordCat = await Category.findById(record.categoryId)
-        record.categoryIconClass = recordCat.faIconClass
-        totalAmount += record.amount
-      })
-    )
+    userRecords.forEach(record => totalAmount += record.amount)
     res.render('index', { categories, userRecords, totalAmount, filterCategoryValue })
   } catch (error) {
     console.log(error)
