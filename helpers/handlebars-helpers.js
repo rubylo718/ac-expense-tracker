e72c76a3e7f7252a06b5e2882a3fd5e58ff8346e
rubylo718 <rubylo718@gmail.com>
2023-04-21 16:52:32 +0800

    fix: add package 'handlebars-helpers' back for the contains function in register page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,8 @@
+const helpers = require('handlebars-helpers')
+const multihelpers = helpers()
+
 module.exports = {
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
-  }
+  }, multihelpers
 }
