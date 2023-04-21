be821fd76ffcfe050eca92c2d476ea02ed4cff21
rubylo718 <rubylo718@gmail.com>
2023-04-21 17:04:15 +0800

    fix: show all record for value of categoryfilter = 0 in index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
     const userId = req.user._id
     const filterCategoryValue = req.query.categoryFilter  
     const userAllRecords = await Record.find({ userId }).populate('userId').populate('categoryId').lean()
-    const userRecords = filterCategoryValue ? userAllRecords.filter(records => records.categoryId.id === filterCategoryValue) : userAllRecords
+    const userRecords = (filterCategoryValue && filterCategoryValue !== '0') ? userAllRecords.filter(records => records.categoryId.id === filterCategoryValue) : userAllRecords
     let totalAmount = 0
     userRecords.forEach(record => totalAmount += record.amount)
     res.render('index', { categories, userRecords, totalAmount, filterCategoryValue })
